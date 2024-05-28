217d0377e78debc356717fb0b36cc9739a5691f9
raushanrauss <151566432+raushanrauss@users.noreply.github.com>
2024-05-28 11:05:37 +0530

    Completed

--- Frontend/src/Sidebar/Sidebar.tsx
+++ Frontend/src/Sidebar/Sidebar.tsx
@@ -7,7 +7,6 @@ import {
   HStack,
   VStack,
   Icon,
- 
   Drawer,
   DrawerContent,
   Text,
@@ -16,10 +15,7 @@ import {
   FlexProps,
   Menu,
   MenuButton,
-  MenuDivider,
-  MenuItem,
-  MenuList,
-} from '@chakra-ui/react';
+} from "@chakra-ui/react";
 import {
   FiBarChart2,
   FiTable,
@@ -27,12 +23,11 @@ import {
   FiSun,
   FiMenu,
   FiBell,
-  FiChevronDown,
-} from 'react-icons/fi';
-import { IconType } from 'react-icons';
-import { NavLink } from 'react-router-dom';
-import { ReactText } from 'react';
-import AllRoutes from '../AllRoutes/AllRoutes';
+} from "react-icons/fi";
+import { IconType } from "react-icons";
+import { NavLink } from "react-router-dom";
+import { ReactText } from "react";
+import AllRoutes from "../AllRoutes/AllRoutes";
 
 interface LinkItemProps {
   name: string;
@@ -41,17 +36,20 @@ interface LinkItemProps {
 }
 
 const LinkItems: Array<LinkItemProps> = [
-  { name: 'Charts', icon: FiBarChart2, path: '/home' },
-  { name: 'Table', icon: FiTable, path: '/table' },
-  { name: 'Reports', icon: FiFileText, path: '/report' },
-  { name: 'ForeCast', icon: FiSun, path: '/forcast' },
+  { name: "Charts", icon: FiBarChart2, path: "/home" },
+  { name: "Table", icon: FiTable, path: "/table" },
+  { name: "Reports", icon: FiFileText, path: "/report" },
+  { name: "ForeCast", icon: FiSun, path: "/forcast" },
 ];
 
 export default function SidebarWithHeader() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <Box className="min-h-screen bg-red-100 dark:bg-gray-900">
-      <SidebarContent onClose={() => onClose} display={{ base: 'none', md: 'block' }} />
+      <SidebarContent
+        onClose={onClose}
+        display={{ base: "none", md: "block" }}
+      />
 
       <Drawer
         autoFocus={false}
@@ -67,7 +65,7 @@ export default function SidebarWithHeader() {
         </DrawerContent>
       </Drawer>
       {/* Mobile nav */}
-      <MobileNav onOpen={onOpen} />
+      <MobileNav onOpen={onOpen} onClose={onClose} />
       <Box className="ml-0 md:ml-60 p-4 mt-20 md:mt-0">
         <AllRoutes />
       </Box>
@@ -87,7 +85,11 @@ const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
     >
       <Flex className="h-20 items-center mx-8 justify-between">
         <Text className="text-2xl font-monospace font-bold">Kaptive</Text>
-        <CloseButton className="flex md:hidden" onClick={onClose} />
+        <CloseButton
+          className="flex md:hidden"
+          onClick={onClose}
+          display={{ base: "flex", md: "none" }} // Display close button only on small screens
+        />
       </Flex>
       {LinkItems.map((link) => (
         <NavItem key={link.name} icon={link.icon} path={link.path}>
@@ -106,17 +108,18 @@ interface NavItemProps extends FlexProps {
 
 const NavItem = ({ icon, children, path, ...rest }: NavItemProps) => {
   return (
-    <NavLink to={path} style={{ textDecoration: 'none' }} className="focus:outline-none">
+    <NavLink
+      to={path}
+      style={{ textDecoration: "none" }}
+      className="focus:outline-none"
+    >
       <Flex
         align="center"
         className="p-4 mx-4 rounded-lg group cursor-pointer hover:bg-cyan-400 hover:text-white"
         {...rest}
       >
         {icon && (
-          <Icon
-            className="mr-4 text-2xl group-hover:text-white"
-            as={icon}
-          />
+          <Icon className="mr-4 text-2xl group-hover:text-white" as={icon} />
         )}
         {children}
       </Flex>
@@ -126,9 +129,10 @@ const NavItem = ({ icon, children, path, ...rest }: NavItemProps) => {
 
 interface MobileProps extends FlexProps {
   onOpen: () => void;
+  onClose: () => void; // Add onClose function prop
 }
 
-const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
+const MobileNav = ({ onOpen,  ...rest }: MobileProps) => {
   return (
     <Flex
       className="ml-0 md:ml-60 px-4 md:px-4 h-20 items-center bg-white dark:bg-gray-900 border-b border-gray-200 dark:border-gray-700 justify-between md:justify-end"
@@ -142,36 +146,35 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
         icon={<FiMenu />}
       />
 
-      <Text className="flex md:hidden text-2xl font-monospace font-bold">Logo</Text>
+      <Text className="flex md:hidden text-2xl font-monospace font-bold">
+        Logo
+      </Text>
 
       <HStack className="space-x-0 md:space-x-6">
-        <IconButton size="lg" variant="ghost" aria-label="open menu" icon={<FiBell />} />
+        <IconButton
+          size="lg"
+          variant="ghost"
+          aria-label="open menu"
+          icon={<FiBell />}
+        />
         <Flex alignItems="center">
           <Menu>
-            <MenuButton py={2} transition="all 0.3s" _focus={{ boxShadow: 'none' }}>
+            <MenuButton
+              py={2}
+              transition="all 0.3s"
+              _focus={{ boxShadow: "none" }}
+            >
               <HStack>
                 <Avatar
                   size="sm"
-                  src={
-                    'https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                  }
+                  src={"https://avatars.githubusercontent.com/u/151566432?v=4"}
                 />
                 <VStack className="hidden md:flex items-start space-y-1 ml-2">
-                  <Text className="text-sm">Justina Clark</Text>
+                  <Text className="text-sm">Raushan Kumar</Text>
                   <Text className="text-xs text-gray-600">Admin</Text>
                 </VStack>
-                <Box className="hidden md:flex">
-                  <FiChevronDown />
-                </Box>
               </HStack>
             </MenuButton>
-            <MenuList className="bg-white dark:bg-gray-900 border-gray-200 dark:border-gray-700">
-              <MenuItem>Profile</MenuItem>
-              <MenuItem>Settings</MenuItem>
-              <MenuItem>Billing</MenuItem>
-              <MenuDivider />
-              <MenuItem>Sign out</MenuItem>
-            </MenuList>
           </Menu>
         </Flex>
       </HStack>
